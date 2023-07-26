ee97362fcfa2011e086de79ff4a6d4dd7566b45f
samuelodeniyi20 <samuelodeniyi2003@gmail.com>
2023-07-26 12:29:42 -0700

    MAKING MINOR CHANGES

--- routes/routes.go
+++ routes/routes.go
@@ -18,7 +18,7 @@ func Routes() {
 	port := "2020"
 
 	r.HandleFunc("/api/user/create", api.CreateUser).Methods("POST")
-	//r.HandleFunc("/api/user/delete/{id}", api.DeleteUser).Methods("GET")
+	r.HandleFunc("/api/user/delete/{id}", api.DeleteUser).Methods("GET")
 	r.HandleFunc("/api/user/get/{id}", api.ReadUser).Methods("GET")
 	//r.HandleFunc("/api/user/login", api.LoginUser).Methods("POST")
 	//r.HandleFunc("/api/user/update/{id}", api.UpdateUser).Methods("POST")
